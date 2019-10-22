25f29dad273ef9349e077e7c8e9f67235cf3a28b
Joakim Lauronen <joakiml@c02nq2y4g3qd.trioptima.local>
2019-10-22 21:05:55 +0200

    removed 20 MA and RSI

--- borsdata_api/stock_screener.py
+++ borsdata_api/stock_screener.py
@@ -3,7 +3,7 @@
 def stock_screener(dataframe):
 
     # f_score_graham > 10
-    dataframe = dataframe[dataframe['f_score_graham'] >= int(0)]
+    dataframe = dataframe[dataframe['f_score_graham'] >= int(11)]
 
     # profit stability over the last 5 year
     dataframe = dataframe[dataframe['profit_stability'] >= int(0)]
@@ -21,10 +21,10 @@ def stock_screener(dataframe):
     dataframe = dataframe[(dataframe['trend1'] >= int(0)) & (dataframe['trend1'] <= int(10))]
 
     # stock in a positive trend? pric/ma20
-    dataframe = dataframe[(dataframe['trend2'] >= int(0)) & (dataframe['trend2'] <= int(10))]
+    #dataframe = dataframe[(dataframe['trend2'] >= int(0)) & (dataframe['trend2'] <= int(10))]
 
     # rsi
-    dataframe = dataframe[(dataframe['rsi'] >= int(10)) & (dataframe['rsi'] <= int(60))]
+    #dataframe = dataframe[(dataframe['rsi'] >= int(10)) & (dataframe['rsi'] <= int(60))]
 
     # ebitda per share data frame > 0 equal to YoY growth
     dataframe = dataframe[(dataframe['ebitda_per_share'] >= int(20))]
@@ -32,9 +32,13 @@ def stock_screener(dataframe):
     # number of outstanding_shares shall be lower than 30M
     dataframe = dataframe[(dataframe['outstanding_shares'] < int(30))]
 
-
     dataframe.sort_values('magic_rank', ascending=True, inplace=True)
+
     dataframe.reset_index(inplace=True)
 
+    if dataframe.empty:
+        print('No stocks mached your criteria')
+        quit()
+    else:
+        return dataframe
 
-    return dataframe
