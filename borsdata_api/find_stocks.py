8756ba55ec300626885d4ee52ffa38350f2d6f0c
Joakim Lauronen <joakimlauronen@Joakims-MacBook-Pro.local>
2022-07-05 23:24:11 +0200

    error handling on api key

--- borsdata_api/find_stocks.py
+++ borsdata_api/find_stocks.py
@@ -5,11 +5,14 @@ def find_stocks(stock_isins, api_key):
     key = api_key
     url = 'https://apiservice.borsdata.se/v1/instruments'
     content = requests.get(url + '?authKey=' + key,
-                           headers={'content-type': 'application/json'})
+                               headers={'content-type': 'application/json'})
+    if content.status_code == 401:
+        print(content.status_code,"Wrong API key")
+        exit()
     data2 = content.json()['instruments']
     df = pd.DataFrame(data2)
     df = df[df['isin'].isin(stock_isins)]
     df = df[df['countryId'] == 1]
     df = df[['insId', 'isin', 'name', 'urlName']]
-
     return df
+
